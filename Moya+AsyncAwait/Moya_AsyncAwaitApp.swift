b5ef6b06cb98adae8d422eb04d375f4db0a3d1b4
una <unagamix@gmail.com>
2023-01-24 21:29:57 +0900

    initial

---
+++ Moya+AsyncAwait/Moya_AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moya_AsyncAwaitApp.swift
+//  Moya+AsyncAwait
+//
+//  Created by unagami on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Moya_AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
