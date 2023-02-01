fc792526accc5f03b68efb5bb11abbfbea37e8d7
una <unagamix@gmail.com>
2023-02-02 00:46:22 +0900

    clean

--- Moya+AsyncAwaitTests/ActivationViewModelTest.swift
+++ Moya+AsyncAwaitTests/ActivationViewModelTest.swift
@@ -8,18 +8,6 @@
 @testable import Moya_AsyncAwait
 import XCTest
 
-private final class ActivationRepositoryMock: ActivationRepositoryProtocol {
-    private(set) var activationCallCount = 0
-    var activationHandler: ((String, String) async throws -> (ActivationEntity))?
-    func activation(code: String, pin: String) async throws -> ActivationEntity {
-        activationCallCount += 1
-        if let activationHandler = activationHandler {
-            return try await activationHandler(code, pin)
-        }
-        fatalError("Not return value")
-    }
-}
-
 final class ActivationViewModelTest: XCTestCase {
     private var activationRepository: ActivationRepositoryMock!
     private var viewModel: ActivationViewModel!
@@ -51,6 +39,20 @@ final class ActivationViewModelTest: XCTestCase {
         XCTAssertEqual(1, activationRepository.activationCallCount)
         XCTAssertEqual(expected.str("code"), actual.str("code"))
         XCTAssertEqual(expected.str("pin"), actual.str("pin"))
+    }
+}
+
+// -------------------------------------------------------------
+
+private final class ActivationRepositoryMock: ActivationRepositoryProtocol {
+    private(set) var activationCallCount = 0
+    var activationHandler: ((String, String) async throws -> (ActivationEntity))?
+    func activation(code: String, pin: String) async throws -> ActivationEntity {
+        activationCallCount += 1
+        if let activationHandler = activationHandler {
+            return try await activationHandler(code, pin)
+        }
+        fatalError("Not return value")
     }
 }
 
