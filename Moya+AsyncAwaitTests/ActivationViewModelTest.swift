b62c1c8b3224155adb7e095e8aafc077b2847c53
una <unagamix@gmail.com>
2023-02-02 00:24:38 +0900

    feat: mock

--- Moya+AsyncAwaitTests/ActivationViewModelTest.swift
+++ Moya+AsyncAwaitTests/ActivationViewModelTest.swift
@@ -9,26 +9,40 @@
 import XCTest
 
 private final class ActivationRepositoryMock: ActivationRepositoryProtocol {
-    func activation(code _: String, pin _: String) async throws -> ActivationEntity {
-        print("# Call by Mock")
-        return ActivationEntity(origin: "DUMMY")
+    private(set) var activationCallCount = 0
+    var activationHandler: ((String, String) async throws -> (ActivationEntity))?
+    func activation(code: String, pin: String) async throws -> ActivationEntity {
+        activationCallCount += 1
+        if let activationHandler = activationHandler {
+            return try await activationHandler(code, pin)
+        }
+        fatalError("Not return value")
     }
 }
 
 final class ActivationViewModelTest: XCTestCase {
+    private var activationRepository: ActivationRepositoryMock!
     private var viewModel: ActivationViewModel!
 
     override func setUpWithError() throws {
+        activationRepository = ActivationRepositoryMock()
         viewModel = ActivationViewModel(
             scheduler: TestScheduler(self),
-            activationRepository: ActivationRepositoryMock()
+            activationRepository: activationRepository
         )
     }
 
     func test_activation() throws {
         // When
+        activationRepository.activationHandler = { pin, code in
+            print("pin: \(pin), code: \(code)")
+            return ActivationEntity(origin: "#Dummy")
+        }
+
         // Given
         viewModel.activation()
+
         // Then
+        XCTAssertEqual(1, activationRepository.activationCallCount)
     }
 }
